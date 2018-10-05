4a4326d3e25af0946636fba2bce95516d91342ee
Ezekiel Mangalu <emangalu@jirnexu.com>
2018-10-05 17:13:38 +0800

    Added greet function

--- handler.js
+++ handler.js
@@ -12,3 +12,14 @@ module.exports.hello = async (event, context) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+module.exports.greet = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Hello offline serverless',
+      input: event,
+      context: context
+    }),
+  }
+};
